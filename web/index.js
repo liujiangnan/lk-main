daf2630d49b33be6dd281dcfc5517925cecfe746
liujiangnan <695541723@qq.com>
2017-11-23 19:48:08 +0800

    主页提交

---
+++ web/index.js
@@ -0,0 +1,24 @@
+ShowWaitting(true); 
+//element框架需要先加载完js才能正常使用
+$.getScript("lk-main/web/lib/element-ui/lib/index.js",function(){
+  ShowWaitting(false);
+  ready();
+}); 
+
+function ready(){
+  new Vue({
+    el: '#app',
+    data: function() {
+      return { 
+        visible: false,
+        activeIndex: '1',
+        activeIndex2: '1'
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  })
+}
