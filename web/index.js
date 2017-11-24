76e2b125fcc2b20abcd870d2774932039b5be80f
liujiangnan <695541723@qq.com>
2017-11-24 18:20:21 +0800

    提交

--- web/index.js
+++ web/index.js
@@ -1,23 +1,27 @@
-ShowWaitting(true); 
+ShowWaitting(true);
 //element框架需要先加载完js才能正常使用
-$.getScript("lk-main/web/lib/element-ui/lib/index.js",function(){
+$.getScript("lk-main/web/lib/element-ui/lib/index.js", function() {
   ShowWaitting(false);
   ready();
-}); 
+});
 
-function ready(){
+function ready() {
   new Vue({
     el: '#app',
     data: function() {
-      return { 
+      return {
         visible: false,
         activeIndex: '1',
-        activeIndex2: '1'
+        activeIndex2: '1',
+        tets: true
       }
     },
     methods: {
       handleSelect(key, keyPath) {
         console.log(key, keyPath);
+      },
+      changeCollapse(){
+        this.tets = this.tets?false:true;
       }
     }
   })
